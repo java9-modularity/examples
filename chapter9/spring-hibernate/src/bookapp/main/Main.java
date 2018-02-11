b42a20feb5ed4193a5219a3c9a4fcd0e6fe3241a
Semyon Gaschenko <semyon.gashchenko@gmail.com>
2018-02-11 20:11:59 +0300

    Fix indentations

--- chapter9/spring-hibernate/src/bookapp/main/Main.java
+++ chapter9/spring-hibernate/src/bookapp/main/Main.java
@@ -18,20 +18,20 @@ public class Main {
     BooksService booksService = context.getBean(BooksService.class);
     BookstoreService store = context.getBean(BookstoreService.class);
 
-      // Create some books
-      int id1 = booksService.createBook("Java 9 Modularity", 45.0d);
-      int id2 = booksService.createBook("Modular Cloud Apps with OSGi", 40.0d);
-      printf("Created books with id [%d, %d]", id1, id2);
+    // Create some books
+    int id1 = booksService.createBook("Java 9 Modularity", 45.0d);
+    int id2 = booksService.createBook("Modular Cloud Apps with OSGi", 40.0d);
+    printf("Created books with id [%d, %d]", id1, id2);
 
-      // Retrieve them again
-      Book book1 = booksService.getBook(id1);
-      Book book2 = booksService.getBook(id2);
-      printf("Retrieved books:\n  %d: %s [%.2f]\n  %d: %s [%.2f]",
+    // Retrieve them again
+    Book book1 = booksService.getBook(id1);
+    Book book2 = booksService.getBook(id2);
+    printf("Retrieved books:\n  %d: %s [%.2f]\n  %d: %s [%.2f]",
         id1, book1.getTitle(), book1.getPrice(), id2, book2.getTitle(), book2.getPrice());
 
-      // Use the other service to calculate a total
-      double total = store.calculatePrice(id1, id2);
-      printf("Total price (with tax): %.2f", total);
+    // Use the other service to calculate a total
+    double total = store.calculatePrice(id1, id2);
+    printf("Total price (with tax): %.2f", total);
 
   }
 
@@ -40,6 +40,6 @@ public class Main {
   }
 
   private void printf(String msg, Object... args) {
-      System.out.println(String.format(msg + "\n", args));
-    }
+    System.out.println(String.format(msg + "\n", args));
+  }
 }
